fee1233bc587474d767ea6ff2b5a609c96d71870
Fatih Güneş <gnsso@outlook.com>
2018-07-05 15:35:49 +0300

    Add mvc client project

---
+++ MeetingOrganizer.Client/Controllers/HomeController.cs
@@ -0,0 +1,157 @@
+﻿using MeetingOrganizer.Server.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetingOrganizer.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string baseUrl = "http://localhost:52507/";
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            using (var client = SetupClient())
+            {
+                using (var response = await client.GetAsync("api/meetings"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        var meetings = JsonConvert.DeserializeObject<List<MeetingModel>>(json);
+                        return View(meetings);
+                    }
+                    else
+                    {
+                        return Redirect("Error");
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            using (var client = SetupClient())
+            {
+                using (var response = await client.GetAsync($"api/meetings/{id}"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        var meeting = JsonConvert.DeserializeObject<MeetingModel>(json);
+                        return View(meeting);
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([System.Web.Http.FromBody]MeetingModel model)
+        {
+            using (var client = SetupClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                using (var response = await client.PostAsync("api/meetings/plan", content))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int id)
+        {
+            using (var client = SetupClient())
+            {
+                using (var response = await client.GetAsync($"api/meetings/{id}"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        var meeting = JsonConvert.DeserializeObject<MeetingModel>(json);
+                        return View(meeting);
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Save([System.Web.Http.FromBody]MeetingModel model)
+        {
+            using (var client = SetupClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                using (var response = await client.PostAsync("api/meetings/update", content))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            using (var client = SetupClient())
+            {
+                using (var response = await client.PostAsync($"api/meetings/{id}/delete", null))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }
+        }
+
+        private HttpClient SetupClient()
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+    }
+}
--- MeetingOrganizer.Server/Models/MeetingModel.cs
+++ MeetingOrganizer.Server/Models/MeetingModel.cs
@@ -9,11 +9,23 @@ namespace MeetingOrganizer.Server.Models
     public class MeetingModel
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required, Display(Name = "Konu")]
         public string Subject { get; set; }
+
+        [Display(Name = "Tarih")]
+        [DisplayFormat(DataFormatString = "{0:ddd, dd.MM.yyyy}", ApplyFormatInEditMode = false)]
         public DateTime Date { get; set; }
+
+        [Display(Name = "Başlangıç Saati")]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = false)]
         public DateTime StartedDate { get; set; }
+
+        [Display(Name = "Bitiş Saati")]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = false)]
         public DateTime FinishedDate { get; set; }
+
+        [Display(Name = "Katılımcılar")]
         public IList<ParticipantModel> Participants { get; set; }
     }
 }
