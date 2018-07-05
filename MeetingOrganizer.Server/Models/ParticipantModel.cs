d821cb031d6c04e4542148ed302f85865278e16f
Fatih Güneş <gnsso@outlook.com>
2018-07-05 15:38:51 +0300

    Display participant name

--- MeetingOrganizer.Server/Models/ParticipantModel.cs
+++ MeetingOrganizer.Server/Models/ParticipantModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace MeetingOrganizer.Server.Models
 {
     public class ParticipantModel
     {
+        [Display(Name = "İsim")]
         public string Name { get; set; }
         public string Email { get; set; }
     }
